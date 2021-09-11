5340f064ff8500b3dfed637b25a77fb03fdf5e7c
timchae <76724557+timchae@users.noreply.github.com>
2021-09-11 23:32:56 +0900

    jpa-shop 예제

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
